86b2c129948a4fc562187ea730cb5164dd77fe13
BreadSoup <79678268+BreadSoup@users.noreply.github.com>
2023-09-17 23:06:14 -0400

    Made it more beginner friendly

--- Main.cs
+++ Main.cs
@@ -1,9 +1,12 @@
-﻿using MelonLoader;
+﻿using BoneLib;
+using MelonLoader;
 
-namespace bonelab_template
+namespace bonelab_template // Must change this to the name of your mod or has a very High chance of causing issues!!!
 {
     internal partial class Main : MelonMod
     {
+        public const string ModName = "Mod Name"; // Change this to the name of your mod
+
         public override void OnEarlyInitializeMelon()
         {
             base.OnEarlyInitializeMelon();
@@ -12,11 +15,20 @@ namespace bonelab_template
         public override void OnInitializeMelon()
         {
             base.OnInitializeMelon();
+            
+            Hooking.OnLevelInitialized += _ => { OnSceneAwake(); }; // This will call the OnSceneAwake() method in Main.cs when the level is initialized
+            Preferences.MelonPreferencesCreator(); // This will call the MelonPreferencesCreator() method in Preferences.cs
+            Preferences.BoneMenuCreator(); // This will call the BoneMenuCreator() method in Preferences.cs
         }
 
         public override void OnLateInitializeMelon()
         {
             base.OnLateInitializeMelon();
+        }
+        
+        public static void OnSceneAwake()
+        {
+            
         }
 
         public override void OnUpdate()
